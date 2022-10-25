d2c381379414db14776a48f60cb59def41e288ac
David Palazon Bermudez <david.palazon@gmail.com>
2022-10-26 00:45:53 +0200

    Subida

---
+++ pruebasTeamcity/pruebasTeamcity/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebasTeamcity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{i}");            
+            }
+
+            Console.WriteLine(Calcular(null,1));
+        }
+
+        private static int? Calcular(int? a, int? b) => a + b;
+    }
+}
